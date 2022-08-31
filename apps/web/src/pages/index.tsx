23cbd8a93787d03db8aabca61e6a0835d11ef6c2
Andre Vitorio <andre@vitorio.net>
2022-08-31 18:55:06 +0200

    feat: add an ok looking home

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -26,34 +26,51 @@ export default function Home({ allPostsData }: HomeProps) {
       </Head>
 
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
-        <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-          <span className="inline-block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent">
-            Outstatic Example
-          </span>{' '}
+        <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl mb-4">
+          Outstatic
         </h1>
+        <h2 className="mx-auto max-w-5xl text-center text-4xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-3xl lg:text-4xl xl:text-4xl">
+          A Static CMS for Next.js
+        </h2>
         <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
           <a href="https://outstatic.com/docs">
             <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-black px-8 py-3 text-base font-medium text-white no-underline hover:bg-gray-700 dark:bg-white dark:text-black dark:hover:bg-gray-300 md:py-3 md:px-10 md:text-lg md:leading-6">
-              Read the docs
+              Get Started
               <span className="ml-2 bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent">
                 →
               </span>
             </div>
           </a>
         </div>
+        <div className="grid grid-cols-3 mt-20">
+          <h3 className="mx-auto max-w-5xl text-center text-3xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-3xl lg:text-4xl xl:text-4xl">
+            Open Source.
+          </h3>
+          <h3 className="mx-auto max-w-5xl text-center text-3xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-3xl lg:text-4xl xl:text-4xl">
+            Host for Free.
+          </h3>
+          <h3 className="mx-auto max-w-5xl text-center text-3xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-3xl lg:text-4xl xl:text-4xl">
+            5-minute setup.
+          </h3>
+        </div>
 
-        <div className="grid grid-cols-2 gap-10 mt-10">
-          {allPostsData.map(({ slug, publishedAt, title, description }) => (
-            <Link key={slug} href={`/posts/${slug}`}>
-              <a className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-                <small>{formatDate(publishedAt)}</small>
-                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                  {title}
-                </h2>
-                <p className="font-normal text-gray-700">{description}</p>
-              </a>
-            </Link>
-          ))}
+        <div className="w-full mx-auto mt-20 bg-blue-500 p-10">
+          <h3 className="text-4xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-3xl lg:text-4xl xl:text-4xl">
+            Our blog
+          </h3>
+          <div className="grid grid-cols-2 gap-10 mt-10">
+            {allPostsData.map(({ slug, publishedAt, title, description }) => (
+              <Link key={slug} href={`/posts/${slug}`}>
+                <a className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+                  <small>{formatDate(publishedAt)}</small>
+                  <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                    {title}
+                  </h2>
+                  <p className="font-normal text-gray-700">{description}</p>
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
       </main>
     </div>
