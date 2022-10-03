ae6180e6951dfd9567b68fa4a6e15dfb653bbe6e
Andre Vitorio <andre@vitorio.net>
2022-10-03 22:22:22 +0200

    style: update settings design

--- packages/outstatic/src/pages/settings.tsx
+++ packages/outstatic/src/pages/settings.tsx
@@ -10,25 +10,27 @@ export default function Settings() {
         <h1 className="mr-12 text-2xl">Settings</h1>
       </div>
       <div className="max-w-lg">
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900">
-            Repository
-          </label>
-          <input
-            className="cursor-not-allowed block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm outline-none"
-            value={repoSlug}
-            readOnly
-          />
-        </div>
-        <div className="mt-4">
-          <label className="block mb-2 text-sm font-medium text-gray-900">
-            Content Path
-          </label>
-          <input
-            className="cursor-not-allowed block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm outline-none"
-            value={`${contentPath}`}
-            readOnly
-          />
+        <div className="mb-20 max-w-2xl p-8 px-4 md:p-8 text-black bg-white rounded-lg border border-gray-200 shadow-md prose prose-base">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              Repository
+            </label>
+            <input
+              className="cursor-not-allowed block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm outline-none"
+              value={repoSlug}
+              readOnly
+            />
+          </div>
+          <div className="mt-4">
+            <label className="block mb-2 text-sm font-medium text-gray-900">
+              Content Path
+            </label>
+            <input
+              className="cursor-not-allowed block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm outline-none"
+              value={`${contentPath}`}
+              readOnly
+            />
+          </div>
         </div>
       </div>
     </AdminLayout>
