146091c7607da9f4344aba7ebfc5737357b28c6f
Andre Vitorio <andre@vitorio.net>
2022-10-05 20:47:50 +0200

    feat: update CodeBlock

--- packages/outstatic/src/components/CodeBlock/index.tsx
+++ packages/outstatic/src/components/CodeBlock/index.tsx
@@ -26,21 +26,25 @@ const CodeBlock = ({
 }: CodeBlockProps) => {
   return (
     <NodeViewWrapper className="relative">
-      <select
-        contentEditable={false}
-        defaultValue={defaultLanguage}
-        className="absolute top-2 right-2 bg-white focus:outline-none rounded-sm"
-        onChange={(event) => updateAttributes({ language: event.target.value })}
-      >
-        <option value="null">auto</option>
-        <option disabled>—</option>
-        {extension.options.lowlight.listLanguages().map((lang, index) => (
-          <option key={index} value={lang}>
-            {lang}
-          </option>
-        ))}
-      </select>
-      <pre className="text-white bg-slate-900 rounded-md p-4">
+      <div className="absolute top-0 right-6 rounded-b-md border border-t-0 border-gray-600 px-3 py-1">
+        <select
+          contentEditable={false}
+          defaultValue={defaultLanguage}
+          className="select-none bg-gradient-to-tr from-primary-300 to-primary-400 bg-clip-text font-medium text-white outline-none text-sm"
+          onChange={(event) =>
+            updateAttributes({ language: event.target.value })
+          }
+        >
+          <option value="null">auto</option>
+          <option disabled>—</option>
+          {extension.options.lowlight.listLanguages().map((lang, index) => (
+            <option key={index} value={lang}>
+              {lang}
+            </option>
+          ))}
+        </select>
+      </div>
+      <pre className="text-white bg-slate-900 rounded-md p-4 pt-12">
         <NodeViewContent as="code" />
       </pre>
     </NodeViewWrapper>
