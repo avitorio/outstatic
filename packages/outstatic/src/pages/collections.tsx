e7553a6051f8c15d49b98430e08b7fcfede5b501
Andre Vitorio <andre@vitorio.net>
2022-10-14 18:35:44 +0200

    style: add blue on hover for collection cards

--- packages/outstatic/src/pages/collections.tsx
+++ packages/outstatic/src/pages/collections.tsx
@@ -108,7 +108,7 @@ export default function Collections() {
                 className="relative flex p-6 justify-between items-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-slate-100"
               >
                 <Link href={`/outstatic/${collection}`}>
-                  <h5 className="text-2xl cursor-pointer font-bold tracking-tight text-gray-900 capitalize">
+                  <h5 className="text-2xl cursor-pointer font-bold tracking-tight text-gray-900 capitalize hover:text-blue-500">
                     {collection}
                     <span className="absolute top-0 bottom-0 left-0 right-16"></span>
                   </h5>
@@ -146,15 +146,15 @@ export default function Collections() {
           }}
         >
           <div className="space-y-6 p-6 text-left">
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            <p className="text-base leading-relaxed text-gray-500">
               Are you sure you want to delete this collection?
             </p>
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            <p className="text-base leading-relaxed text-gray-500">
               This action cannot be undone.
             </p>
           </div>
 
-          <div className="flex items-center space-x-2 rounded-b border-t border-gray-200 p-6 dark:border-gray-600">
+          <div className="flex items-center space-x-2 rounded-b border-t p-6">
             <button
               type="button"
               className="flex rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none"
@@ -193,7 +193,7 @@ export default function Collections() {
             </button>
             <button
               type="button"
-              className="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-600"
+              className="rounded-lg border border-gray-200 px-5 py-2.5 text-sm font-medium focus:z-10 focus:outline-none focus:ring-4 order-gray-600 bg-gray-800 text-white hover:border-gray-600 hover:bg-gray-700 focus:ring-gray-700"
               onClick={() => {
                 setShowDeleteModal(false)
                 setSelectedCollection('')
