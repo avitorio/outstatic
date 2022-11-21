9d140acd7b00b7c72cbe2df9673dd2f72716b5be
Jakob Heuser <jakob@codedrift.com>
2022-11-21 10:56:43 -0800

    revert: Removes changes to eslint-config-custom

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -5,7 +5,7 @@ module.exports = {
     jest: true,
     node: true
   },
-  extends: ['next', 'turbo'],
+  extends: ['next', 'turbo', 'prettier'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off'
