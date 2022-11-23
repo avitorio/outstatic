6191d96aba0b37f4f9f94a5a77648c5737f54697
Jakob Heuser <jakob@codedrift.com>
2022-11-22 17:13:17 -0800

    Creates initial metadata graphql for rebuild

--- packages/outstatic/src/pages/settings.tsx
+++ packages/outstatic/src/pages/settings.tsx
@@ -11,6 +11,22 @@ export default function Settings() {
       </div>
       <div className="max-w-lg">
         <div className="mb-8 max-w-2xl p-8 px-4 md:p-8 text-black bg-white rounded-lg border border-gray-200 shadow-md prose prose-base">
+          <h2>Metadata</h2>
+          <button
+            disabled
+            className="cursor-pointer rounded-lg border px-5 py-2.5 text-sm font-medium focus:outline-none focus:ring-4 border-gray-600 bg-gray-800 text-white hover:border-gray-600 hover:bg-gray-700 focus:ring-gray-700 no-underline"
+          >
+            Rebuild Metadata
+          </button>
+          <p className="text-sm">
+            If you&apos;ve made changes outside of outstatic, or if you are
+            seeing old posts, you can rebuild your metadata database and
+            automatically deploy those changes to your site.
+          </p>
+        </div>
+
+        <div className="mb-8 max-w-2xl p-8 px-4 md:p-8 text-black bg-white rounded-lg border border-gray-200 shadow-md prose prose-base">
+          <h2>Environment</h2>
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Repository
@@ -41,19 +57,19 @@ export default function Settings() {
               readOnly
             />
           </div>
+          <p className="text-sm">
+            To learn more about how to update these values,{' '}
+            <a
+              href="https://outstatic.com/docs/environment-variables"
+              target="_blank"
+              rel="noreferrer"
+              className="underline font-semibold"
+            >
+              click here
+            </a>
+            .
+          </p>
         </div>
-        <p>
-          To learn more about how to update these values,{' '}
-          <a
-            href="https://outstatic.com/docs/environment-variables"
-            target="_blank"
-            rel="noreferrer"
-            className="underline font-semibold"
-          >
-            click here
-          </a>
-          .
-        </p>
       </div>
     </AdminLayout>
   )
