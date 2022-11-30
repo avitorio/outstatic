d2f8effde9ea547206f059f86355f718adcba921
Jakob Heuser <jakob@codedrift.com>
2022-11-29 16:24:48 -0800

    feat: Adds metadata builder which can generate a commit payload

---
+++ packages/outstatic/src/components/MetadataBuilder/index.tsx
@@ -0,0 +1,216 @@
+import React, {
+  HTMLAttributes,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from 'react'
+import { OutstaticContext } from '../../context'
+import {
+  DocumentDocument,
+  DocumentQuery,
+  DocumentQueryVariables,
+  GetFileInformationQuery,
+  useGetFileInformationQuery
+} from '../../graphql/generated'
+import { DeepNonNullable } from '../../types'
+import { useApollo } from '../../utils/apollo'
+import matter from 'gray-matter'
+import { chunk } from '../../utils/chunk'
+import { hashFromUrl } from '../../utils/hashFromUrl'
+import useOid from '../../utils/useOid'
+import { createCommitInput } from '../../utils/createCommitInput'
+
+interface MetadataBuilderProps extends HTMLAttributes<HTMLDivElement> {
+  rebuild: boolean
+  onComplete?: () => void
+}
+
+/** Describes the extraction of commit data from the gql query */
+interface FileData {
+  path: string
+  oid: string
+  commit: string
+}
+
+/** Describes the metadata format */
+interface FileMetadata {
+  [key: string]: unknown
+  __outstatic: {
+    path: string
+    hash: string
+  }
+}
+
+export const MetadataBuilder: React.FC<MetadataBuilderProps> = ({
+  rebuild,
+  onComplete,
+  ...rest
+}) => {
+  const [total, setTotal] = useState(0)
+  const [processed, setProcessed] = useState(0)
+  const client = useApollo()
+  const fetchOid = useOid()
+
+  const { repoOwner, repoSlug, repoBranch, contentPath, monorepoPath } =
+    useContext(OutstaticContext)
+
+  const rootPath = [monorepoPath, contentPath].filter(Boolean).join('/')
+
+  const { data } = useGetFileInformationQuery({
+    variables: {
+      owner: repoOwner,
+      name: repoSlug,
+      expression: `${repoBranch}:${rootPath}`
+    },
+    skip: !rebuild
+  })
+
+  const files = useMemo(() => {
+    // strip object freeze to work with mutable data
+    // https://github.com/apollographql/apollo-client/issues/5987#issuecomment-590938556
+    const o = JSON.parse(
+      JSON.stringify(data?.repository?.object ?? {})
+    ) as DeepNonNullable<GetFileInformationQuery>['repository']['object']
+
+    const output: FileData[] = []
+    const queue = 'entries' in o ? o.entries ?? [] : []
+
+    while (queue.length > 0) {
+      const next = queue.pop()
+      if (next?.object?.__typename === 'Tree') {
+        // subdir - add entries to queue
+        queue.push(...(next.object.entries ?? []))
+      } else if (next?.object?.__typename === 'Blob') {
+        // file - add to output
+        output.push({
+          path: next.path,
+          oid: `${next.object.oid}`,
+          commit: hashFromUrl(`${next.object.commitUrl}`)
+        })
+      }
+    }
+    // clone out of Apollo's forced read-only
+    return output
+  }, [data])
+
+  // using useEffect ensures we run a single processing loop
+  useEffect(() => {
+    const takeAndProcess = async (o: FileData) => {
+      const res = await client.query<DocumentQuery, DocumentQueryVariables>({
+        query: DocumentDocument,
+        variables: {
+          owner: repoOwner,
+          name: repoSlug,
+          filePath: `${repoBranch}:${o.path}`
+        }
+      })
+
+      if (res.data.repository?.object?.__typename === 'Blob') {
+        const m = matter(res.data.repository.object.text ?? '')
+        const fmd: FileMetadata = {
+          ...m.data,
+          __outstatic: {
+            hash: o.commit,
+            path: o.path
+          }
+        }
+        return fmd
+      }
+
+      return undefined
+    }
+
+    const fn = async () => {
+      setTotal(Math.max(files.length, 1))
+
+      const chunkSize = 5 // TODO move to constants
+      const queue = chunk(files, chunkSize)
+      const docs: Record<string, unknown>[] = []
+      const pendingOid = fetchOid()
+
+      // process in chunks
+      while (queue.length > 0) {
+        const next = queue.pop()
+        if (!next) continue
+        const all = Promise.allSettled(
+          next.map(async (fd) => {
+            const meta = await takeAndProcess(fd)
+            docs.push({
+              ...meta,
+              category: fd.path
+                .replace(rootPath, '') // strip root
+                .replace(/^\/+/, '') // strip leading slashes
+                .replace(/\/.+$/, '') // strip all after 1st slash
+            })
+            setProcessed((prev) => prev + 1)
+          })
+        )
+        await all // lets fn() throw on a bad chunk
+      }
+
+      // await now that chunks are done in background
+      const oid = await pendingOid
+
+      if (docs.length > 0 && oid) {
+        const parentHash = hashFromUrl(
+          data?.repository?.object?.__typename === 'Tree'
+            ? data.repository.object.commitUrl
+            : ''
+        )
+        const payload = createCommitInput({
+          message: 'chore: Updates metadata DB',
+          owner: repoOwner,
+          repoSlug,
+          repoBranch,
+          oid,
+          contentPath,
+          monorepoPath
+        })
+        const db = {
+          hash: parentHash,
+          generated: new Date().toUTCString(),
+          metadata: docs.filter(Boolean)
+        }
+        payload.input.fileChanges.additions.push({
+          path: `${
+            monorepoPath ? monorepoPath + '/' : ''
+          }${contentPath}/metadata.json`,
+          contents: JSON.stringify(db)
+        })
+
+        // TODO execute commit mutation
+        console.log('PAYLOAD', payload)
+      }
+    }
+
+    fn().catch(console.error)
+  }, [
+    client,
+    data?.repository?.object,
+    files,
+    repoBranch,
+    repoOwner,
+    repoSlug,
+    rootPath,
+    fetchOid,
+    contentPath,
+    monorepoPath
+  ])
+
+  useEffect(() => {
+    if (processed === total && onComplete) {
+      onComplete()
+    }
+  }, [onComplete, processed, total])
+
+  if (!rebuild) {
+    return <div {...rest} />
+  }
+
+  return (
+    <div {...rest}>
+      {processed}&nbsp;/&nbsp;{total}
+    </div>
+  )
+}
--- packages/outstatic/src/pages/settings.tsx
+++ packages/outstatic/src/pages/settings.tsx
@@ -1,157 +1,13 @@
-import React, {
-  HTMLAttributes,
-  useContext,
-  useEffect,
-  useMemo,
-  useRef,
-  useState
-} from 'react'
+import { useContext, useState } from 'react'
 import { clsx } from 'clsx'
 import { AdminLayout } from '../components'
 import { OutstaticContext } from '../context'
-import {
-  DocumentDocument,
-  DocumentQuery,
-  DocumentQueryVariables,
-  GetFileInformationQuery,
-  useGetFileInformationQuery
-} from '../graphql/generated'
-import { DeepNonNullable } from '../types'
-import { useApollo } from '../utils/apollo'
-import matter from 'gray-matter'
-
-interface MetadataBuilderProps extends HTMLAttributes<HTMLDivElement> {
-  rebuild: boolean
-  onComplete?: () => void
-}
-
-interface FileData {
-  path: string
-  oid: string
-  commit: string
-}
-
-const MetadataBuilder: React.FC<MetadataBuilderProps> = ({
-  rebuild,
-  onComplete,
-  ...rest
-}) => {
-  const [total, setTotal] = useState(0)
-  const [processed, setProcessed] = useState(0)
-  const client = useApollo()
-
-  const { repoOwner, repoSlug, repoBranch, contentPath, monorepoPath } =
-    useContext(OutstaticContext)
-
-  const rootPath = [monorepoPath, contentPath].filter(Boolean).join('/')
-
-  const { data } = useGetFileInformationQuery({
-    variables: {
-      owner: repoOwner,
-      name: repoSlug,
-      expression: `${repoBranch}:${rootPath}`
-    },
-    skip: !rebuild
-  })
-
-  const files = useMemo(() => {
-    // https://github.com/apollographql/apollo-client/issues/5987#issuecomment-590938556
-    const o = JSON.parse(
-      JSON.stringify(data?.repository?.object ?? {})
-    ) as DeepNonNullable<GetFileInformationQuery>['repository']['object']
-
-    const output: FileData[] = []
-    const queue = 'entries' in o ? o.entries ?? [] : []
-
-    while (queue.length > 0) {
-      const next = queue.pop()
-      if (next?.object?.__typename === 'Tree') {
-        // subdir - add entries to queue
-        queue.push(...(next.object.entries ?? []))
-      } else if (next?.object?.__typename === 'Blob') {
-        // file - add to output
-        output.push({
-          path: next.path,
-          oid: `${next.object.oid}`,
-          commit: `${next.object.commitUrl}`
-        })
-      }
-    }
-    return output
-  }, [data])
-
-  // using useEffect ensures we run a single processing loop
-  useEffect(() => {
-    // TODO: concurrent
-    const takeAndProcess = async (o: FileData) => {
-      const res = await client.query<DocumentQuery, DocumentQueryVariables>({
-        query: DocumentDocument,
-        variables: {
-          owner: repoOwner,
-          name: repoSlug,
-          filePath: `${repoBranch}:${o.path}`
-        }
-      })
-
-      if (res.data.repository?.object?.__typename === 'Blob') {
-        const m = matter(res.data.repository.object.text ?? '')
-        return m.data
-      }
-
-      return undefined
-    }
-
-    const fn = async () => {
-      setTotal(Math.max(files.length, 1))
-
-      // TODO: Read existing JSON DB off of github if it exists
-
-      const queue = JSON.parse(JSON.stringify(files)) as FileData[]
-      const docs: Record<string, unknown>[] = []
-
-      while (queue.length > 0) {
-        const next = queue.pop()
-        if (next) {
-          const meta = await takeAndProcess(next)
-          docs.push({
-            ...meta,
-            category: next.path
-              .replace(rootPath, '')
-              .replace(/^\/+/, '')
-              .replace(/\/.+$/, '')
-          })
-          setProcessed((prev) => prev + 1)
-        }
-      }
-
-      // TODO: create JSON db and schedule commit
-      if (docs.length > 0) {
-        console.log('METADATA EXRACT', docs.filter(Boolean))
-      }
-    }
-    fn().catch(console.error)
-  }, [client, files, repoBranch, repoOwner, repoSlug, rootPath])
-
-  useEffect(() => {
-    if (processed === total && onComplete) {
-      onComplete()
-    }
-  }, [onComplete, processed, total])
-
-  if (!rebuild) {
-    return <div {...rest} />
-  }
-
-  return (
-    <div {...rest}>
-      {processed}&nbsp;/&nbsp;{total}
-    </div>
-  )
-}
+import { MetadataBuilder } from '../components/MetadataBuilder'
 
 export default function Settings() {
   const [rebuild, setRebuilding] = useState(false)
   const { repoSlug, repoBranch, contentPath } = useContext(OutstaticContext)
+
   return (
     <AdminLayout title="Settings">
       <div className="mb-8 flex h-12 items-center">
@@ -180,8 +36,8 @@ export default function Settings() {
           </div>
           <p className="text-sm">
             If you&apos;ve made changes outside of outstatic, or if you are
-            seeing old posts, you can rebuild your metadata database and
-            automatically deploy those changes to your site.
+            seeing posts with incorrect metadata, you can rebuild your metadata
+            and automatically deploy those changes to your site.
           </p>
         </div>
 
@@ -218,7 +74,8 @@ export default function Settings() {
             />
           </div>
           <p className="text-sm">
-            To learn more about how to update these values,{' '}
+            These values come from your Outstatic environment. To learn more
+            about how to update these values,{' '}
             <a
               href="https://outstatic.com/docs/environment-variables"
               target="_blank"
