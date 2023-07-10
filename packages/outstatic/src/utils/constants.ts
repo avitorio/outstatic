35ea4f3ccb62dc19032f8a24a143cec59b5d16f8
Andre Vitorio <andre@vitorio.net>
2023-07-10 05:40:23 +0200

    chore: upgrade outstatic to 0.0.36-canary.9

--- packages/outstatic/src/utils/constants.ts
+++ packages/outstatic/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const OUTSTATIC_VERSION = '0.0.36-canary.8'
+export const OUTSTATIC_VERSION = '0.0.36-canary.9'
 export const OUTSTATIC_URL = 'https://outstatic.com'
 export const IMAGES_PATH = 'images/'
