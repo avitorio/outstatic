761bec627dabfe7248d5b0b848f4c7705b919f7a
Andre Vitorio <andre@vitorio.net>
2023-07-28 15:21:57 +0200

    test: add test to Sidebar

--- packages/outstatic/src/components/Sidebar/test.tsx
+++ packages/outstatic/src/components/Sidebar/test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { OutstaticProvider } from '../../context'
-import Sidebar from '.'
+import Sidebar from './'
+import { act } from 'react-dom/test-utils'
 
 const providerData = {
   repoOwner: 'anything',
@@ -10,20 +11,80 @@ const providerData = {
   monorepoPath: 'anything',
   session: null,
   initialApolloState: null,
-  collections: ['posts', 'docs'],
+  collections: ['collection1', 'collection2', 'collection3'],
   pages: [],
   addPage: (page: string) => {},
   removePage: (page: string) => {}
 }
 
+jest.mock('next/link', () => {
+  return ({ children }: any) => {
+    return children
+  }
+})
+
+jest.mock('js-cookie', () => ({
+  get: jest.fn(() => null),
+  set: jest.fn()
+}))
+
+jest.mock('../../utils/generateUniqueId', () => jest.fn())
+
 describe('<Sidebar />', () => {
-  it('should render the heading', () => {
-    render(
-      <OutstaticProvider {...providerData}>
-        <Sidebar isOpen={false} />
-      </OutstaticProvider>
+  const mockCollections = ['collection1', 'collection2', 'collection3']
+
+  const fetchMock = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          title: 'Test Title',
+          content: 'Test Content',
+          link: 'Test Link'
+        })
+    })
+  )
+
+  global.fetch = fetchMock as jest.Mock
+
+  it('should render the component correctly', async () => {
+    await act(async () => {
+      render(
+        <OutstaticProvider {...providerData}>
+          <Sidebar isOpen={true} />
+        </OutstaticProvider>
+      )
+    })
+
+    expect(screen.getByLabelText('Sidebar')).toBeInTheDocument()
+    expect(screen.getByText('Collections')).toBeInTheDocument()
+    expect(screen.getByText('Settings')).toBeInTheDocument()
+    expect(screen.getByText('Documentation')).toBeInTheDocument()
+
+    mockCollections.forEach((collection) => {
+      expect(screen.getByText(collection)).toBeInTheDocument()
+    })
+  })
+
+  it('should fetch broadcast data and set it as cookie', async () => {
+    await act(async () => {
+      render(
+        <OutstaticProvider {...providerData}>
+          <Sidebar isOpen={true} />
+        </OutstaticProvider>
+      )
+    })
+
+    expect(fetchMock).toHaveBeenCalled()
+    expect(jest.requireMock('js-cookie').set).toHaveBeenCalledWith(
+      'ost_broadcast',
+      JSON.stringify({
+        title: 'Test Title',
+        content: 'Test Content',
+        link: 'Test Link'
+      }),
+      {
+        expires: 1
+      }
     )
-    expect(screen.getByText(/Posts/i)).toBeInTheDocument()
-    expect(screen.getByText(/Settings/i)).toBeInTheDocument()
   })
 })
