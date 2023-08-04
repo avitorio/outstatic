eb2864be5d6a3768c855003157c1d5721d57cf4b
anthony <quereantho@gmail.com>
2023-08-04 22:14:11 +0200

    refactor(oauth-scopes): use a URL object to create the github url

--- packages/outstatic/src/app/api/auth/login.ts
+++ packages/outstatic/src/app/api/auth/login.ts
@@ -3,9 +3,11 @@ import { redirect } from 'next/navigation'
 export default async function GET() {
   const scopes = ['read:user', 'repo']
 
-  redirect(
-    `https://github.com/login/oauth/authorize?client_id=${
-      process.env.OST_GITHUB_ID
-    }&scope=${scopes.join('%20')}&response_type=code`
-  )
+  const url = new URL('https://github.com/login/oauth/authorize')
+
+  url.searchParams.append('client_id', process.env.OST_GITHUB_ID ?? '')
+  url.searchParams.append('scopes', scopes.join(' '))
+  url.searchParams.append('response_type', 'code')
+
+  redirect(url.toString())
 }
