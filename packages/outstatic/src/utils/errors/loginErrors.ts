e32220b345bc39e490b82a74812226d68d840661
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2023-10-05 23:12:00 +0200

    Add check repo function (#141)
    Co-authored-by: Andre Vitorio <andre@vitorio.net>

--- packages/outstatic/src/utils/errors/loginErrors.ts
+++
@@ -1,6 +0,0 @@
-const loginErrors = {
-  something: 'Something went wrong. Please try again.',
-  'not-collaborator': 'You are not a collaborator of this repository.'
-}
-
-export default loginErrors
