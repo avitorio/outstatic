537b0124a3ace2c5dde11edecf9d711626408d30
Andre Vitorio <andre@vitorio.net>
2023-10-06 12:49:33 +0200

    Enter prerelease mode and version packages

--- packages/outstatic/src/utils/constants.ts
+++ packages/outstatic/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const OUTSTATIC_VERSION = '1.0.0-canary.1'
+export const OUTSTATIC_VERSION = '1.0.0-canary.2'
 export const OUTSTATIC_URL = 'https://outstatic.com'
 export const IMAGES_PATH = 'images/'
