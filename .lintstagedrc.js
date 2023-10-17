e8d94262507e2a09d50b193ec276bd2a939f186d
Andre Vitorio <andre@vitorio.net>
2023-10-17 15:11:28 +0200

    refactor: remove tailwind-config package

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,11 @@
 module.exports = {
   '(!README)*.(md|json|graphql)': 'prettier --write',
   ...[
-    'apps/dev13',
     'apps/dev',
-    'apps/dev13',
     'apps/web',
     'examples/blog',
     'packages/eslint-custom-config',
     'packages/outstatic',
-    'packages/tailwind-config',
     'packages/tsconfig'
   ].reduce((actions, pkg) => {
     actions[`${pkg}/**/*.{cjs,mjs,js,jsx,ts,tsx}`] = [
