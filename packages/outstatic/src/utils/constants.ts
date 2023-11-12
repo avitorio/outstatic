b4c9bf61cfdbba9f70538de6076beee2e3b99596
Andre Vitorio <andre@vitorio.net>
2023-11-12 22:25:50 +0100

    chore: upgrade to 1.0.2-canary.2

--- packages/outstatic/src/utils/constants.ts
+++ packages/outstatic/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const OUTSTATIC_VERSION = '1.0.2-canary.1'
+export const OUTSTATIC_VERSION = '1.0.2-canary.2'
 export const OUTSTATIC_URL = 'https://outstatic.com'
 export const IMAGES_PATH = 'images/'
 export const API_IMAGES_PATH = 'api/outstatic/images/'
