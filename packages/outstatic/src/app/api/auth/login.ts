e7cc55c0e8f2afbc34480aa2db3782a77ae36283
Slawa Pidgorny <spidgorny@gmail.com>
2023-11-26 22:02:09 +0100

    Change env to OST_GITHUB_CALLBACK_URL

--- packages/outstatic/src/app/api/auth/login.ts
+++ packages/outstatic/src/app/api/auth/login.ts
@@ -8,8 +8,8 @@ export default async function GET() {
   url.searchParams.append('client_id', process.env.OST_GITHUB_ID ?? '')
   url.searchParams.append('scope', scopes.join(','))
   url.searchParams.append('response_type', 'code')
-  if (process.env?.NEXT_PUBLIC_APP_URL) {
-    url.searchParams.append('redirect_uri', process.env?.NEXT_PUBLIC_APP_URL)
+  if (process.env?.OST_GITHUB_CALLBACK_URL) {
+    url.searchParams.append('redirect_uri', process.env?.OST_GITHUB_CALLBACK_URL)
   }
 
   redirect(url.toString())
