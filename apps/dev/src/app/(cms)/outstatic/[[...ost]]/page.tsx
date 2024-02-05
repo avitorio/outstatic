0c380fa4473406e4e4ff9ad758f7c7a91ff1a807
Andre Vitorio <andre@vitorio.net>
2024-02-05 10:23:27 -0300

    feat: add title to outstatic pages

--- apps/dev/src/app/(cms)/outstatic/[[...ost]]/page.tsx
+++ apps/dev/src/app/(cms)/outstatic/[[...ost]]/page.tsx
@@ -1,6 +1,6 @@
-import 'outstatic/outstatic.css'
 import { Outstatic } from 'outstatic'
 import { OstClient } from 'outstatic/client'
+import 'outstatic/outstatic.css'
 
 export default async function Page({ params }: { params: { ost: string[] } }) {
   const ostData = await Outstatic()
