888b44340218733ff9f4025204c9cfe9925f24ea
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2024-03-02 11:58:32 +0100

    feat: add basePath property to context

--- apps/dev/next.config.js
+++ apps/dev/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['outstatic']
+  transpilePackages: ['outstatic'],
+  basePath: '/base-path'
 }
 
 module.exports = nextConfig
