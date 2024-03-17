135fb89abaf18424f9344e6444ef7221a73dabd3
Andre Vitorio <andre@vitorio.net>
2024-03-17 01:24:09 -0300

    refactor: remove unnecessary files

--- apps/docs/src/app/[slug]/page.tsx
+++ apps/docs/src/app/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header'
+import Header from '@/components/header'
 import formatDate from '@/lib/formatDate'
 import { Metadata } from 'next'
 import { getDocumentBySlug, getDocumentSlugs, load } from 'outstatic/server'
