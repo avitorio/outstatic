e9a4a1e438e5667f8e4224fa213807781b8bae22
Andre Vitorio <andre@vitorio.net>
2024-03-17 05:34:06 -0300

    chore: remove test basePath

--- apps/dev/next.config.js
+++ apps/dev/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['outstatic'],
-  basePath: '/base-path'
+  transpilePackages: ['outstatic']
 }
 
 module.exports = nextConfig
