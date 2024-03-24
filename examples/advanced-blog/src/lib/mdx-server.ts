effaa88aa97f6da3d93703ff9a38bf4d9a75af7e
Andre Vitorio <andre@vitorio.net>
2024-03-24 20:24:37 -0300

    feat: add table support to examples

--- examples/advanced-blog/src/lib/mdx-server.ts
+++ examples/advanced-blog/src/lib/mdx-server.ts
@@ -1,34 +1,39 @@
-import { bundleMDX } from "mdx-bundler";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
-import rehypeSlug from "rehype-slug";
+import { bundleMDX } from 'mdx-bundler'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
+import remarkGfm from 'remark-gfm'
 
 export default async function MDXServer(code: string) {
   const result = await bundleMDX({
     source: code,
     mdxOptions(options) {
-      options.rehypePlugins = [
-        ...(options.rehypePlugins ?? []),
-        rehypeSlug,
-        [
-          rehypePrettyCode,
-          {
-            theme: "dracula",
-            // The rest of the rehypePrettyCode config
-          },
-        ],
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: {
-              className: ["hash-anchor"],
-            },
-          },
-        ],
-      ];
-      return options;
-    },
-  });
+      ;(options.remarkPlugins = [
+        ...(options.remarkPlugins ?? []),
+        remarkGfm // Adding remarkGfm here
+      ]),
+        (options.rehypePlugins = [
+          ...(options.rehypePlugins ?? []),
+          rehypeSlug,
+          [
+            rehypePrettyCode,
+            {
+              theme: 'dracula'
+              // The rest of the rehypePrettyCode config
+            }
+          ],
+          [
+            rehypeAutolinkHeadings,
+            {
+              properties: {
+                className: ['hash-anchor']
+              }
+            }
+          ]
+        ])
+      return options
+    }
+  })
 
-  return result.code;
+  return result.code
 }
--- examples/docs/src/lib/mdx-server.ts
+++ examples/docs/src/lib/mdx-server.ts
@@ -2,30 +2,35 @@ import { bundleMDX } from 'mdx-bundler'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
+import remarkGfm from 'remark-gfm'
 
 export default async function MDXServer(code: string) {
   const result = await bundleMDX({
     source: code,
     mdxOptions(options) {
-      options.rehypePlugins = [
-        ...(options.rehypePlugins ?? []),
-        rehypeSlug,
-        [
-          rehypePrettyCode,
-          {
-            theme: 'dracula'
-            // The rest of the rehypePrettyCode config
-          }
-        ],
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: {
-              className: ['hash-anchor']
+      ;(options.remarkPlugins = [
+        ...(options.remarkPlugins ?? []),
+        remarkGfm // Adding remarkGfm here
+      ]),
+        (options.rehypePlugins = [
+          ...(options.rehypePlugins ?? []),
+          rehypeSlug,
+          [
+            rehypePrettyCode,
+            {
+              theme: 'dracula'
+              // The rest of the rehypePrettyCode config
             }
-          }
-        ]
-      ]
+          ],
+          [
+            rehypeAutolinkHeadings,
+            {
+              properties: {
+                className: ['hash-anchor']
+              }
+            }
+          ]
+        ])
       return options
     }
   })
