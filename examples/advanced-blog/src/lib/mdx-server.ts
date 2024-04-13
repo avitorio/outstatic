48ab58196661f6581d5cf9f1954c42597f7806dc
Andre Vitorio <andre@vitorio.net>
2024-04-13 18:01:42 -0300

    refactor: adjust code style (we should probably add a linter to the examples)

--- examples/advanced-blog/src/lib/mdx-server.ts
+++ examples/advanced-blog/src/lib/mdx-server.ts
@@ -1,36 +1,36 @@
-import { bundleMDX } from 'mdx-bundler';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypePrettyCode from 'rehype-pretty-code';
-import rehypeSlug from 'rehype-slug';
-import remarkGfm from 'remark-gfm';
+import { bundleMDX } from 'mdx-bundler'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
+import remarkGfm from 'remark-gfm'
 
 export default async function MDXServer(code: string) {
   const result = await bundleMDX({
     source: code,
     mdxOptions(options) {
-      options.remarkPlugins = options.remarkPlugins ?? [];
-      options.remarkPlugins.push(remarkGfm as any);
-      
-      options.rehypePlugins = options.rehypePlugins ?? [];
-      options.rehypePlugins.push(rehypeSlug as any);
+      options.remarkPlugins = options.remarkPlugins ?? []
+      options.remarkPlugins.push(remarkGfm as any)
+
+      options.rehypePlugins = options.rehypePlugins ?? []
+      options.rehypePlugins.push(rehypeSlug as any)
       options.rehypePlugins.push([
         rehypePrettyCode as any,
         {
-          theme: 'dracula', // Add other rehypePrettyCode options if necessary
-        },
-      ]);
+          theme: 'dracula' // Add other rehypePrettyCode options if necessary
+        }
+      ])
       options.rehypePlugins.push([
         rehypeAutolinkHeadings as any,
         {
           properties: {
-            className: ['hash-anchor'],
-          },
-        },
-      ]);
+            className: ['hash-anchor']
+          }
+        }
+      ])
 
-      return options;
-    },
-  });
+      return options
+    }
+  })
 
-  return result.code;
+  return result.code
 }
