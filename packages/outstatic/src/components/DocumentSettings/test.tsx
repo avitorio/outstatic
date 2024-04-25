69783210bed108661cf6e671513f6f25d854cd68
Andre Vitorio <andre@vitorio.net>
2024-04-25 13:19:13 -0300

    test: fix DocumentSettings test

--- packages/outstatic/src/components/DocumentSettings/test.tsx
+++ packages/outstatic/src/components/DocumentSettings/test.tsx
@@ -52,19 +52,21 @@ describe('<DocumentSettings />', () => {
     })
 
     render(
-      <TestProviders.DocumentContext
-        value={{
-          document: { ...documentExample, author: { name: undefined } }
-        }}
-      >
-        <TestProviders.Form>
-          <DocumentSettings
-            saveFunc={() => {}}
-            loading={false}
-            showDelete={false}
-          />
-        </TestProviders.Form>
-      </TestProviders.DocumentContext>
+      <TestWrapper>
+        <TestProviders.DocumentContext
+          value={{
+            document: { ...documentExample, author: { name: undefined } }
+          }}
+        >
+          <TestProviders.Form>
+            <DocumentSettings
+              saveFunc={() => {}}
+              loading={false}
+              showDelete={false}
+            />
+          </TestProviders.Form>
+        </TestProviders.DocumentContext>
+      </TestWrapper>
     )
     expect(screen.getByLabelText('Name')).toHaveValue('')
     expect(screen.getByLabelText('Name')).not.toBeUndefined()
