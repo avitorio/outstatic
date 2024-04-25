174a5dff484e2f9873fcff6a4cf058f9a6276c56
Andre Vitorio <andre@vitorio.net>
2024-04-25 17:45:27 -0300

    feat: use context for initial data

--- packages/outstatic/src/client/pages/index.tsx
+++ packages/outstatic/src/client/pages/index.tsx
@@ -2,7 +2,8 @@
 import { OutstaticData } from '@/app'
 import { AdminHeader, Sidebar } from '@/components'
 import { AdminLoading } from '@/components/AdminLoading'
-import useOutstatic, { useInitialData } from '@/utils/hooks/useOutstatic'
+import { InitialDataContext } from '@/utils/hooks/useInitialData'
+import useOutstatic from '@/utils/hooks/useOutstatic'
 import { queryClient } from '@/utils/react-query/queryClient'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
@@ -13,13 +14,11 @@ import Onboarding from './onboarding'
 import Welcome from './welcome'
 
 export const Client = ({
-  ostData,
   params
 }: {
   ostData: OutstaticData
   params: { ost: string[] }
 }) => {
-  useInitialData(ostData)
   const { repoSlug, repoOwner, repoBranch, isPending } = useOutstatic()
   const [openSidebar, setOpenSidebar] = useState(false)
   const toggleSidebar = () => {
@@ -30,6 +29,7 @@ export const Client = ({
     <div id="outstatic">
       <AdminHeader toggleSidebar={toggleSidebar} />
       <div className="flex md:grow flex-col-reverse justify-between md:flex-row md:min-h-[calc(100vh-56px)]">
+        {' '}
         <div className="flex w-full">
           <Sidebar isOpen={openSidebar} />
           {isPending ? (
@@ -58,12 +58,13 @@ export const OstClient = (props: {
   if (!ostData?.session) {
     return <Login />
   }
+
   return (
-    <>
+    <InitialDataContext.Provider value={ostData}>
       <Toaster richColors />
       <QueryClientProvider client={queryClient}>
         <Client {...props} />
       </QueryClientProvider>
-    </>
+    </InitialDataContext.Provider>
   )
 }
