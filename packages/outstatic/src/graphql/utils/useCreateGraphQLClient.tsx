1b85ec40dcdbf2feb9a716a66e83567585cadd94
Andre Vitorio <andre@vitorio.net>
2024-08-28 16:36:19 -0300

    feat: fetch csrf from outstatic

---
+++ packages/outstatic/src/graphql/utils/useCreateGraphQLClient.tsx
@@ -0,0 +1,21 @@
+import { GraphQLClient } from 'graphql-request'
+import { GITHUB_GQL_API_URL } from '@/utils/constants'
+import { useCsrfToken } from '@/utils/hooks/useCsrfToken'
+
+type HeadersType = {
+  authorization: string
+  'X-CSRF-Token'?: string
+}
+
+export function useCreateGraphQLClient(
+  githubGql: string,
+  headers: HeadersType
+): GraphQLClient {
+  const csrfToken = useCsrfToken()
+
+  if (githubGql !== GITHUB_GQL_API_URL && csrfToken) {
+    headers['X-CSRF-Token'] = csrfToken
+  }
+
+  return new GraphQLClient(githubGql, { headers })
+}
