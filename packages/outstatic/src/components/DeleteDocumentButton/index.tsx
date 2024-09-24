7179e588ec833303451ee407fc2b5ac0d13d5aa6
Andre Vitorio <andre@vitorio.net>
2024-09-24 12:38:05 -0300

    update delete button modal and icon

--- packages/outstatic/src/components/DeleteDocumentButton/index.tsx
+++ packages/outstatic/src/components/DeleteDocumentButton/index.tsx
@@ -7,7 +7,7 @@ import { useGetMetadata } from '@/utils/hooks/useGetMetadata'
 import useOid from '@/utils/hooks/useOid'
 import useOutstatic from '@/utils/hooks/useOutstatic'
 import { stringifyMetadata } from '@/utils/metadata/stringify'
-import { Trash2 } from 'lucide-react'
+import { Trash } from 'lucide-react'
 import { useState } from 'react'
 import { Button } from '@/components/ui/shadcn/button'
 
@@ -88,7 +88,7 @@ const DeleteDocumentButton = ({
         variant="ghost"
       >
         <span className="sr-only">Delete document</span>
-        <Trash2 className="stroke-foreground" />
+        <Trash className="stroke-foreground" />
       </Button>
       {showDeleteModal && (
         <Modal title="Delete Document" close={() => setShowDeleteModal(false)}>
@@ -100,7 +100,10 @@ const DeleteDocumentButton = ({
               This action cannot be undone.
             </p>
           </div>
-          <div className="flex items-center space-x-2 rounded-b border-t p-6">
+          <div className="flex items-center space-x-2 rounded-b border-t p-6 justify-end">
+            <Button variant="outline" onClick={() => setShowDeleteModal(false)}>
+              Cancel
+            </Button>
             <Button
               variant="destructive"
               onClick={() => {
@@ -109,34 +112,12 @@ const DeleteDocumentButton = ({
             >
               {deleting ? (
                 <>
-                  <svg
-                    className="mr-3 -ml-1 h-5 w-5 animate-spin text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
+                  <Trash className="w-6 h-6" />
                   Deleting
                 </>
               ) : (
                 'Delete'
               )}
-            </Button>
-            <Button variant="outline" onClick={() => setShowDeleteModal(false)}>
-              Cancel
             </Button>
           </div>
         </Modal>
