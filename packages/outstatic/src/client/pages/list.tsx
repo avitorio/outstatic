aa6b36ffa8e9b281c05c95749d1d3288565262a0
Andre Vitorio <andre@vitorio.net>
2024-10-04 19:09:03 -0300

    improve ui

--- packages/outstatic/src/client/pages/list.tsx
+++ packages/outstatic/src/client/pages/list.tsx
@@ -67,36 +67,42 @@ export default function List({ collection }: ListProps) {
       {data?.documents.length === 0 && !isPending && (
         <LineBackground>
           <div className="relative">
-            <div className="mb-20 max-w-2xl p-8 px-4 md:p-8 text-black bg-white rounded-lg border border-gray-200 shadow-md prose prose-base">
-              <h3>This collection has no documents yet.</h3>
-              <p>
-                Create your first{' '}
-                <span className="capitalize font-semibold">
-                  {singular(sentenceCase(collection))}
-                </span>{' '}
-                by clicking the button below.
-              </p>
+            <Card className="mb-20 max-w-2xl animate-fade-in">
+              <CardHeader>
+                <CardTitle>This collection has no documents yet.</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col w-full gap-4 prose prose-base">
+                <p>
+                  Create your first{' '}
+                  <span className="capitalize font-semibold">
+                    {singular(sentenceCase(collection))}
+                  </span>{' '}
+                  by clicking the button below.
+                </p>
 
-              <Button asChild>
-                <Link
-                  href={`${dashboardRoute}/${collection}/new`}
-                  className="no-underline capitalize"
-                >
-                  New {singular(sentenceCase(collection))}
-                </Link>
-              </Button>
-              <p>
-                To learn more about how documents work{' '}
-                <a
-                  href="https://outstatic.com/docs/introduction#whats-a-document"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  click here
-                </a>
-                .
-              </p>
-            </div>
+                <div>
+                  <Button asChild>
+                    <Link
+                      href={`${dashboardRoute}/${collection}/new`}
+                      className="no-underline capitalize"
+                    >
+                      New {singular(sentenceCase(collection))}
+                    </Link>
+                  </Button>
+                </div>
+                <p className="mt-4">
+                  To learn more about how documents work{' '}
+                  <a
+                    href="https://outstatic.com/docs/introduction#whats-a-document"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    click here
+                  </a>
+                  .
+                </p>
+              </CardContent>
+            </Card>
           </div>
         </LineBackground>
       )}
