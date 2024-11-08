498330a5ab5ea5c5fac1ff06866e9536421d49ff
Andre Vitorio <andre@vitorio.net>
2024-11-07 23:02:30 -0300

    chore: update version constant

--- packages/outstatic/src/utils/constants.ts
+++ packages/outstatic/src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { Document } from '@/types'
 
 // Outstatic configuration
-export const OUTSTATIC_VERSION = '1.4.9'
+export const OUTSTATIC_VERSION = '1.5.0-canary.0'
 export const OUTSTATIC_URL = 'https://outstatic.com'
 
 // API configuration
