b4ab4a801f11848f373fb3f4c9a5a93215608a30
Andre Vitorio <andre@vitorio.net>
2024-11-08 16:06:42 -0300

    chore: v1.5.0-canary.2

--- packages/outstatic/postbuild.js
+++ packages/outstatic/postbuild.js
@@ -1,20 +1,25 @@
 const fs = require('fs')
 const path = require('path')
 
-const filePath = path.join(__dirname, './dist/client/client.js')
+const files = [
+  path.join(__dirname, './dist/client/client.js'),
+  path.join(__dirname, './dist/client/client.mjs')
+]
 
-fs.readFile(filePath, 'utf8', function (err, data) {
-  if (err) {
-    return console.error(err)
-  }
-
-  const result = data.replace(/"use strict";/g, '"use client";')
-
-  fs.writeFile(filePath, result, 'utf8', function (err) {
+files.forEach((filePath) => {
+  fs.readFile(filePath, 'utf8', function (err, data) {
     if (err) {
       return console.error(err)
     }
 
-    console.log(`Modified file: ${filePath}`)
+    const result = data.replace(/"use strict";/g, '"use client";')
+
+    fs.writeFile(filePath, result, 'utf8', function (err) {
+      if (err) {
+        return console.error(err)
+      }
+
+      console.log(`Modified file: ${filePath}`)
+    })
   })
 })
