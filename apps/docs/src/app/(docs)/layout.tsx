bcf20d7f3008c69dab2ef2446ba92b4db7649a67
Andre Vitorio <andre@vitorio.net>
2024-11-09 15:27:54 -0300

    chore: upgrade docs to next 15

--- apps/docs/src/app/(docs)/layout.tsx
+++ apps/docs/src/app/(docs)/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         {process.env.NEXT_PUBLIC_TINYBIRD_TOKEN ? (
           <Script
