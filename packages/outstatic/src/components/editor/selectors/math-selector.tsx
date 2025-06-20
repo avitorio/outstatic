b3647cd73c124c6c476ca37040df4056dfc48339
Andre Vitorio <andre@vitorio.net>
2025-06-20 14:11:26 -0300

    style: add dark mode to bubble menu

--- packages/outstatic/src/components/editor/selectors/math-selector.tsx
+++ packages/outstatic/src/components/editor/selectors/math-selector.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/shadcn/button'
 import { cn } from '@/utils/ui'
 import { SigmaIcon } from 'lucide-react'
 import { useEditor } from '@/components/editor/editor-context'
+import { EditorBubbleButton } from '../ui/editor-bubble-button'
 
 export const MathSelector = () => {
   const { editor } = useEditor()
@@ -9,12 +10,9 @@ export const MathSelector = () => {
   if (!editor) return null
 
   return (
-    <Button
-      variant="ghost"
-      size="sm"
-      className="rounded-none w-12"
-      onClick={(evt) => {
-        evt.preventDefault()
+    <EditorBubbleButton
+      name="math"
+      onSelect={() => {
         if (editor.isActive('math')) {
           editor.chain().focus().unsetLatex().run()
         } else {
@@ -31,6 +29,6 @@ export const MathSelector = () => {
         className={cn('size-4', { 'text-blue-500': editor.isActive('math') })}
         strokeWidth={2.3}
       />
-    </Button>
+    </EditorBubbleButton>
   )
 }
