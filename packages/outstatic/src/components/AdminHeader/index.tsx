d0c676a13266c9388d63f2deecff9d890a0a572b
Andre Vitorio <andre@vitorio.net>
2025-06-27 18:14:24 -0300

    feat: add community to admin header

--- packages/outstatic/src/components/AdminHeader/index.tsx
+++ packages/outstatic/src/components/AdminHeader/index.tsx
@@ -6,7 +6,8 @@ import {
   LogOut,
   Monitor,
   Moon,
-  Sun
+  Sun,
+  BookTextIcon
 } from 'lucide-react'
 import { memo, useEffect, useState } from 'react'
 import { Button } from '@/components/ui/shadcn/button'
@@ -32,6 +33,7 @@ import {
 import { useTheme } from 'next-themes'
 import Link from 'next/link'
 import { useSidebar } from '../ui/shadcn/sidebar'
+import { DiscordLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
 
 const themes = [
   {
@@ -45,6 +47,40 @@ const themes = [
   {
     icon: Sun,
     name: 'light'
+  }
+]
+
+const community = [
+  {
+    icon: GitHubLogoIcon,
+    name: 'GitHub',
+    href: 'https://github.com/avitorio/outstatic'
+  },
+  {
+    icon: () => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 300 300.251"
+        fill="none"
+        width="15"
+        height="13"
+        style={{
+          height: '13px'
+        }}
+      >
+        <path
+          fill="currentColor"
+          d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"
+        />
+      </svg>
+    ),
+    name: 'X',
+    href: 'https://x.com/outstatic'
+  },
+  {
+    icon: DiscordLogoIcon,
+    name: 'Discord',
+    href: 'https://discord.gg/myn4qgfXaG'
   }
 ]
 
@@ -132,11 +168,39 @@ const AdminHeader = () => {
                     <Link
                       className="flex cursor-pointer items-center space-x-2"
                       href="https://outstatic.com/docs"
+                      target="_blank"
+                      rel="noreferrer"
                     >
-                      <MessageCircleQuestion className={'h-5'} />
+                      <BookTextIcon className={'h-5'} />
                       <span>Documentation</span>
                     </Link>
                   </DropdownMenuItem>
+
+                  <DropdownMenuSeparator />
+
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                      <div className="focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 flex cursor-pointer items-center space-x-2 flex cursor-pointer items-center space-x-2">
+                        <MessageCircleQuestion className={'h-5'} />
+                        <span>Community</span>
+                      </div>
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuSubContent>
+                      {community.map((t) => (
+                        <DropdownMenuItem key={t.name} asChild>
+                          <Link
+                            className="flex cursor-pointer items-center space-x-2"
+                            href={t.href}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <t.icon className={'h-5'} />
+                            <span className="capitalize">{t.name}</span>
+                          </Link>
+                        </DropdownMenuItem>
+                      ))}
+                    </DropdownMenuSubContent>
+                  </DropdownMenuSub>
 
                   <DropdownMenuSeparator />
 
