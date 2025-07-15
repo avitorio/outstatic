3e297daa601c44fc8f3e77d9d9e3cd7ad2d41377
Andre Vitorio <andre@vitorio.net>
2025-07-15 12:02:04 -0300

    chore: update constant to 2.0.9

--- packages/outstatic/src/utils/constants.ts
+++ packages/outstatic/src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { Document } from '@/types'
 
 // Outstatic configuration
-export const OUTSTATIC_VERSION = '2.0.8'
+export const OUTSTATIC_VERSION = '2.0.9'
 export const OUTSTATIC_URL = 'https://outstatic.com'
 
 // API configuration
