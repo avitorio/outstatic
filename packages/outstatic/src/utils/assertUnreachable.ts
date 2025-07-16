5bc5c9a4034733633719d6e1c84d1f56cc8afa2b
Andre Vitorio <andre@vitorio.net>
2025-07-16 16:57:09 -0300

    refactor: remove unused file

--- packages/outstatic/src/utils/assertUnreachable.ts
+++
@@ -1,5 +0,0 @@
-export function assertUnreachable(_x: never): never {
-  throw new Error(
-    `Reached a code that should be unreachable. This can happen, when a TS type is incorrectly defined.`
-  )
-}
