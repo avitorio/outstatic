572331061bc5918affc338c9d316974a3abac19b
Andre Vitorio <andre@vitorio.net>
2025-12-03 18:25:56 -0300

    chore: remove error log

--- packages/outstatic/src/utils/auth/auth.ts
+++ packages/outstatic/src/utils/auth/auth.ts
@@ -298,7 +298,6 @@ export async function getLoginSession(): Promise<LoginSession | null> {
   const token = cookieStore.get(TOKEN_NAME)?.value
 
   if (!token) {
-    console.error('No token found')
     return null
   }
 
