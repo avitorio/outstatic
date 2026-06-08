265579dfefc109c65f2fa59a2dbb5b95e4f79379
Andre Vitorio <andre@vitorio.net>
2026-06-08 13:00:24 -0300

    chore: use outstatic from workspace

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -4,6 +4,7 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  transpilePackages: ['outstatic'],
   reactStrictMode: true,
   basePath: '/docs',
   images: {
